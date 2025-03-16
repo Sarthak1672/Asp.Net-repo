23e7135cfd4be49e227f7a76e8506a545d5def54
Sarthak1672 <deokarsarthak081@gmail.com>
2025-03-16 19:03:11 +0530

    Initial commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderProcessingApp.Models;
+
+namespace OrderProcessingApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessOrder(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.CalculateDiscount();
+                return View("Result", order);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderProcessingApp.Models
+{
+    public class Order
+    {
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Order amount must be greater than zero.")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string CustomerType { get; set; }
+
+        public decimal Discount { get; set; }
+        public decimal FinalTotal => Amount - Discount;
+
+        public void CalculateDiscount()
+        {
+            if (Amount >= 100 && CustomerType == "Loyal")
+            {
+                Discount = Amount * 0.10m;
+            }
+            else
+            {
+                Discount = 0;
+            }
+        }
+    }
+}
